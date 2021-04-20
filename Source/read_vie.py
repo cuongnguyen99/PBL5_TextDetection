0f525c9327802306b80ca6f6770c6bcfcac97fe7
cuongnguyen99 <kugayuma231@gmail.com>
2021-04-20 16:37:33 +0700

    add img, read text file

---
+++ Source/read_vie.py
@@ -0,0 +1,26 @@
+#import packet
+from PIL import Image
+import pytesseract
+import cv2
+import re
+
+#############################Version 2#########################
+# img = cv2.imread("D:\\Save\\viet2.png")
+img = cv2.imread("D:\\Save\\Project\\viet10.png")
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+text = pytesseract.image_to_string(rgb, lang="vie")
+
+###########################################
+text = text.strip()
+# text = text.split('\n\n')
+# z = '\n'.join(map(str, text))
+# pattern = re.compile('\n')
+# text = text.strip()
+# text = text.replace("  ", " ")
+# text = re.sub(pattern, " ", text)
+###########################################
+
+print(text)
+cv2.imshow("image",rgb)
+cv2.waitKey(0)
+
