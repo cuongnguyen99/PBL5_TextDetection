da242a06e987ff8c6811f465f14aac77a8e5597a
tangphuc <tangbaphuc2906@gmail.com>
2021-04-26 15:55:37 +0700

    tang phuc

---
+++ Source/apiServer.py
@@ -0,0 +1,21 @@
+import requests
+
+# defining the api-endpoint
+API_ENDPOINT = "http://localhost:8080/apis/orders/send-data"
+
+# your API key here
+API_KEY = "XXXXXXXXXXXXXXXXX"
+
+# data to be sent to api
+data = {'orderCode': "abc1222",
+        'name': 'paste',
+        'subPrice': '15000',
+        'shipPrice': '5000',
+        'totalPrice': '30000'}
+
+# sending post request and saving response as response object
+r = requests.post(url=API_ENDPOINT, headers={'token': 'abcxyz'}, data=data)
+
+# extracting response text
+pastebin_url = r.text
+print("The pastebin URL is:%s" % pastebin_url)
