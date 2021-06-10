2ff4b4253fe73f0a6c9e4b34c67fa3ba10c6e5ac
tangphuc <tangbaphuc2906@gmail.com>
2021-06-10 22:30:34 +0700

    add webservice

--- Source/apiServer.py
+++ Source/apiServer.py
@@ -1,17 +1,14 @@
 import requests
 
 # defining the api-endpoint
-API_ENDPOINT = "http://localhost:8080/apis/orders/send-data"
+API_ENDPOINT = "http://localhost:8080/apis/orders"
 
 # your API key here
 API_KEY = "XXXXXXXXXXXXXXXXX"
 
 # data to be sent to api
-data = {'orderCode': "abc1222",
-        'name': 'paste',
-        'subPrice': '15000',
-        'shipPrice': '5000',
-        'totalPrice': '30000'}
+# data={"khu vực": "", "người nhận": "", "số điện thoại": "", "tiền thu hộ": "", "địa chỉ": "", "nội dung": ""}
+data={"area": "Đại Lộc", "receiver": "Tăng Phuc", "phone": "0366061247 ", "price": "4500000", "address": "266/11 Truong Chinh", "content": "het no"}
 
 # sending post request and saving response as response object
 r = requests.post(url=API_ENDPOINT, headers={'token': 'abcxyz'}, data=data)
